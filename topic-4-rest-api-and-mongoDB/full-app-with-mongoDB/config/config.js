b662c659ec516c9964009a003a8e47c75e013738
Marta <118316713+martapolii@users.noreply.github.com>
2024-11-22 15:27:12 -0500

    Remove node_modules from tracking

--- topic-4-rest-api-and-mongoDB/full-app-with-mongoDB/config/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3000,
-  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-  mongoUri: process.env.MONGODB_URI || "mongodb+srv://mpolish3:<password>@comp229.x3pkj.mongodb.net/COMP229?retryWrites=true&w=majority&appName=COMP229"
-};
-export default config;
